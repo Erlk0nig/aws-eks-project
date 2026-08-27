006c40353a8b46e499e68ee525512e50c516f14a
Erlk0nig <soudaniaziz06@gmail.com>
2026-08-27 21:16:39 +0100

    Configue CICD

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.61.0"
+    }
+  }
+  required_version = "1.16.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
