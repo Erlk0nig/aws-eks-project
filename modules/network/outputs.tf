477465cce2ecdbd02223decfd30a372407e433d2
Erlk0nig <soudaniaziz06@gmail.com>
2026-09-07 16:24:56 +0100

    Fix outputs typo

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -5,7 +5,7 @@ output "vpc_id" {
 
 output "private_subnet_ids" {
   description = "List of private subnet IDs" 
-  value       = aws_subnet.private.*.id
+  value       = aws_subnet.private[*].id
 }
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
